e63c92d5199a83d7b289c454651af4c659563f7d
okakakikioAxe <lukmanmuhamadilham@gmail.com>
2026-01-26 09:28:10 +0700

    add navbar en and id language file

---
+++ app/Language/en/Navbar.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'product' => 'Product',
+    'galery' => 'Galery',
+    'contact_us' => 'Contact Us',
+    'about_us' => 'About Us',
+];
---
+++ app/Language/id/Navbar.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'home' => 'Beranda',
+    'product' => 'Produk',
+    'galery' => 'Galeri',
+    'contact_us' => 'Hubungi Kami',
+    'about_us' => 'Tentang Kami',
+];
